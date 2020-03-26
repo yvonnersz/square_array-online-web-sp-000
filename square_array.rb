27aa1a6024a68c509241d566a795b39d7b2ce665
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-26 02:40:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,10 @@
 def square_array(array)
   # your code here
-  array.collect {|x| x*x}
+  new_array = []
+  array.each do |x|
+    squared_x = x*x
+    new_array.push(squared_x)
+  end
+
+  return new_array
 end
